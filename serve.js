3986f4384ccbbf671f31c86f4d65625bdb9ec8a9
freya0608 <freya001@yeah.net>
2017-05-04 01:13:32 +0800

    serve

--- serve.js
+++ serve.js
@@ -18,3 +18,28 @@ function sendFile(response,filePath,fileContents) {
     );
     response.end(fileContents);
 }
+
+function serveStatic(response, cache, absPath) {
+    if(cache[absPath]){//检查文件是否缓存在内存中
+        sendFile(response,absPath,cache[absPath]);//从内存中返回文件
+    }else {
+        fs.exists(absPath,function (exists) {//检查文件是否存在
+            if(exists){
+                fs.readFile(absPath,function (err, data) {//从硬盘中读取文件
+                    if(err){
+                        send404(response);
+                    }else {
+                        cache[absPath] = data;
+                        sendFile(response,absPath,data);//从硬盘中读取文件并返回
+                    }
+                });
+            }else {
+                send404(response);
+            }
+        });
+    }
+}
+
+
+
+
